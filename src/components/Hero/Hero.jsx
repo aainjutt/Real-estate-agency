61d04cad567139984cd68cb116c7f33448d9625a
Aain Ur Rehman <aain134@gmail.com>
2024-01-17 12:10:56 +0500

    Add Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,29 +1,44 @@
 import React from 'react'
 import './Hero.css'
 import hero from '../../assets/hero-image.png'
+import { HiLocationMarker } from "react-icons/hi";
+import CountUp from 'react-countup';
 function Hero() {
-  return (
-    <section className="hero-wrapper">
-        <div className="paddings innerWidth flexCenter hero-container ">
-            <div className="hero-left">
-                <div className="hero-title">
-                    <h1>
-                    Discover <br/>Most Suitable <br/> Property
-                    </h1>
-                    <div className="hero-des">
-                        <span>Find a variety of properties that suit you very easilty
-Forget all difficulties in finding a residence for you</span>
-                    </div>
-                </div>
-            </div>
-            <div className="flexCenter hero-right">
-<div className="image-container">
-    <img src={hero} alt="" />
-</div>
-            </div>
-        </div>
-    </section>
-  )
+    return (
+        <section className="hero-wrapper">
+            <div className="paddings innerWidth flexCenter hero-container">
+                <div className="flexColStart hero-left">
+                    <div className="hero-title">
+                        <div className="orange-circle"/>
+                        <h1 style={{opacity: '1', transform: 'none'}}>Discover <br/>Most Suitable<br/> Property</h1>
+                        </div><div className="flexColStart secondaryText flexhero-des">
+                            <span className='secondaryText'>Find a variety of properties that suit you very easilty</span>
+                            <span className='secondaryText'>Forget all difficulties in finding a residence for you</span></div>
+                        <div className="flexCenter search-bar">
+                          <HiLocationMarker color = 'var(--blue) 'size ={25}/>
+
+                        <input type="text"/><button className="button">Search</button>
+                        </div><div className="flexCenter stats">
+                            <div className="flexColCenter stat">
+                                <span>
+                                  <CountUp start = {8800} end ={9000} duration={4}/>
+                                  <span>+</span>
+                                    </span>
+                                    <span className="secondaryText">Premium Product</span>
+                                    </div><div className="flexColCenter stat"><span>
+                                    <CountUp start = {1950} end ={2000} duration={4}/> <span>+</span></span>
+                                        <span className="secondaryText">Happy Customer</span>
+                                        </div><div className="flexColCenter stat"><span>
+                                        <CountUp  end ={28} /> <span>+</span></span>
+                                            <span className="secondaryText">Awards Winning</span></div></div></div>
+                                            <div className="flexCenter hero-right">
+                                                <div className="image-container" style={{opacity: '1', transform: 'none'}}>
+                            <img src={hero} alt="houses"/>
+                            </div>
+                            </div>
+                            </div>
+                            </section>
+    )
 }
 
 export default Hero
