67b784e6fa2bc646bb3c4e4b3eb14333fcc04940
Aain Ur Rehman <aain134@gmail.com>
2024-01-18 11:21:29 +0500

    Add value

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero/Hero'
 import'./App.css'
 import Companies from './components/Companies/Companies'
 import Residencies from './components/Residencies/Residencies'
+import Value from './components/Value/Value'
 function App() {
   return (
     <div className='App'>
@@ -14,6 +15,7 @@ function App() {
       </div>
       <Companies/>
      <Residencies/>
+     <Value/>
     </div>
   )
 }
---
+++ src/components/Value/Value.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import './Value.css'
+import value from '../../assets/value.png'
+import { Accordion, AccordionItem, AccordionItemButton, AccordionItemHeading, AccordionItemPanel, AccordionItemState } from 'react-accessible-accordion'
+import data from '../../utils/accordin'
+import { MdOutlineArrowDropDown } from 'react-icons/md'
+function Value() {
+    const [className, setClassName]=useState(null)
+
+    return (
+        <section className="v-wrapper">
+            <div className="paddings innerWidth flexCenter v-container">
+                <div className="v-left">
+                    <div className="image-container">
+                        <img src={value} alt="" />
+
+                    </div>
+                </div>
+                <div className="flexColStart v-right">
+                    <span className='orangeText'>
+                        Our Value
+                        </span >
+                        <span className='primaryText'>Our Value We Give To You</span>
+                        <span className='secondaryText'>We always ready to help by providijng the best services for you.
+<br/>We beleive a good blace to live can make your life better</span>
+<Accordion
+className='accordian'
+allowMultipleExpanded={false}
+preExpanded={[0]}
+>
+{
+    data.map((item,i)=>{
+        return (
+            <AccordionItem className={`accordianItem ${className}`} key={i} uuid={i}>
+                <AccordionItemHeading>
+                    <AccordionItemButton className='flexCenter accordianButton'>
+
+                        <AccordionItemState>
+                            {({expanded})=>expanded ? setClassName('expanded'):setClassName('collapsed')}
+                        </AccordionItemState>
+                        <div className="flexCenter icon">
+                            {item.icon}
+                        </div>
+                        <span className="primaryText">
+                            {item.heading}
+                        </span>
+                        <div className="flexCenter icon">
+                            <MdOutlineArrowDropDown size={20}/>
+                        </div>
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                    <p className="secondaryText">{item.detail}</p>
+                </AccordionItemPanel>
+            </AccordionItem>
+        )
+    })
+}
+</Accordion>
+                </div>
+            </div>
+        </section>
+     
+    )
+}
+
+export default Value
